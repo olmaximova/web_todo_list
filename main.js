53fd45d7c87f18d2b3e226325eefb8e4ac896add
olmaximova <ol.maximova1309@gmail.com>
2025-09-30 22:34:05 +0300

    feat: add task to localStorage

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const loadElements = () => {
+    const main = document.createElement('main');
+    document.querySelector('body').append(main);
+
     const section = document.createElement('section');
-    document.querySelector('body').append(section);
+    document.querySelector('main').append(section);
 
     const h1 = document.createElement('h1');
     h1.textContent = 'To Do List';
@@ -11,9 +14,18 @@ const loadElements = () => {
 
     const input = document.createElement('input');
     input.setAttribute('type', 'text');
-    input.setAttribute('placeholder', 'Write anything you want to do');
+    input.setAttribute('id', 'inputTask');
+    input.setAttribute('placeholder', 'Enter a task');
     input.setAttribute('autocomplete', 'off');
+
+    const inputDate = document.createElement('input');
+    inputDate.setAttribute('type', 'date');
+    inputDate.setAttribute('id', 'inputDate');
+    inputDate.setAttribute('placeholder', 'Choose date');
+    inputDate.setAttribute('autocomplete', 'off');
+
     document.querySelector('form').append(input);
+    document.querySelector('form').append(inputDate);
 
     const addButton = document.createElement('button');
     addButton.setAttribute('id', 'add-button');
@@ -23,6 +35,28 @@ const loadElements = () => {
 
     document.querySelector('form').append(addButton);
     document.querySelector('#add-button').append(addImg);
+
+    const ul = document.createElement('ul');
+    ul.setAttribute('id', 'todo-list');
+    document.querySelector('section').append(ul);
+
+    addButton.addEventListener('click', addTask);
+
+}
+
+const taskLocalList = JSON.parse(localStorage.getItem('taskLocalList')) || [];
+
+const addTask = () => {
+    const task = document.querySelector('#inputTask').value.trim();
+    const date = document.querySelector('#inputDate').value;
+
+    taskLocalList.push({
+        task: task,
+        date: date,
+        completed: false
+    });
+
+    localStorage.setItem('taskLocalList', JSON.stringify(taskLocalList));
 }
 
 document.addEventListener('DOMContentLoaded', loadElements)
