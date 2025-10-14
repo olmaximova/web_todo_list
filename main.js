5d55a37349c1e928841ed7218df958aa6a5922f5
olmaximova <ol.maximova1309@gmail.com>
2025-10-14 16:39:45 +0300

    replace icons and create sidebar.js

--- main.js
+++ main.js
@@ -56,196 +56,6 @@ const loadElements = () => {
     styleForm(form);
 }
 
-const createSidebar = () => {
-    const nav = document.createElement('nav');
-    nav.className = 'sidebar';
-
-    const header = document.createElement('header');
-    header.className = 'sidebarHeader'
-
-    const logo = document.createElement('div');
-    logo.className = 'sidebarLogo'
-
-    const logoText = document.createElement('div');
-    logoText.textContent = 'ToDo List';
-
-    const logoImg = document.createElement('img');
-    logoImg.src = 'images/favicon.png';
-
-    logo.append(logoImg, logoText);
-
-    header.append(logo);
-
-    const menuBar = document.createElement('div');
-    menuBar.className = 'sidebarMenu';
-
-    const menuItems = [
-        { id: 'add-task', text: 'add task', icon: 'fa-solid fa-plus'},
-        { id: 'search-task', text: 'search task', icon: 'fa-solid fa-magnifying-glass' },
-        { id: 'view-due-today', text: 'due today', icon: 'fa-regular fa-calendar' },
-        { id: 'view-upcoming', text: 'upcoming', icon: 'fa-regular fa-calendar-days' },
-    ];
-
-    menuItems.forEach(item => {
-        const menuItem = document.createElement('div');
-        menuItem.className = 'sidebarMenuItem';
-        menuItem.id = item.id;
-        
-        const menuIcon = document.createElement('span');
-        menuIcon.className = 'menuIcon';
-        menuIcon.className = item.icon;
-        
-        const menuText = document.createElement('span');
-        menuText.className = 'menuText';
-        menuText.textContent = item.text;
-        
-        menuItem.append(menuIcon, menuText);
-        menuBar.append(menuItem);
-
-        addMenuItemEventListeners(menuItem, item.id);
-    });
-
-    nav.append(header, menuBar);
-
-    styleSidebar(nav);
-    styleSidebarHeader(header);
-    styleSidebarLogo(logo);
-    styleSideBarImg(logoImg);
-    styleSidebarMenu(menuBar);
-    const menuItemElements = nav.querySelectorAll('.sidebarMenuItem');
-    styleSidebarMenuItem(menuItemElements);
-    return nav;
-}
-
-function addMenuItemEventListeners(menuItem, itemID){
-    menuItem.addEventListener('click', () => {
-        switch(itemID) {
-            case 'add-task':
-                openAddTaskModal();
-                break;
-            case 'search-task':
-                openSearchModal();
-                break;
-            case 'view-due-today':
-                showDueTodayTasks();
-                break;
-            case 'view-upcoming':
-                showUpcomingTasks();
-                break;
-        }
-    })
-}
-
-const openAddTaskModal = () => {
-    const modal = document.createElement('div');
-    modal.className = 'addTaskModal';
-    modal.id = 'add-task-modal';
-    
-    const modalInner = document.createElement('div');
-    modalInner.className = 'modalInner';
-    
-    const modalTitle = document.createElement('h2');
-    modalTitle.textContent = 'Add New Task';
-
-    const taskForm = document.createElement('form');
-    taskForm.id = 'task-form';
-    
-    const taskInputGroup = document.createElement('div');
-    taskInputGroup.className = 'formElements';
-    
-    const taskLabel = document.createElement('label');
-    taskLabel.setAttribute('for', 'inputTask');
-    taskLabel.textContent = 'Enter a task:';
-    
-    const taskInput = document.createElement('input'); 
-    taskInput.type = 'text';
-    taskInput.id = 'inputTask';
-    taskInput.setAttribute('autocomplete', 'off');
-    taskInput.setAttribute('required', 'true');
-    taskInput.placeholder = 'Enter your task here';
-    
-    taskInputGroup.append(taskLabel, taskInput);
-    
-    const dateGroup = document.createElement('div');
-    dateGroup.className = 'formElements';
-    
-    const dateLabel = document.createElement('label');
-    dateLabel.setAttribute('for', 'inputDate');
-    dateLabel.textContent = 'Due Date:';
-    
-    const inputDate = document.createElement('input');
-    inputDate.type = 'date';
-    inputDate.id = 'inputDate';
-    inputDate.name = 'task-date';
-    inputDate.required = true;
-    inputDate.setAttribute('autocomplete', 'off');
-    
-    dateGroup.append(dateLabel, inputDate);    
-
-    const formButtons = document.createElement('div');
-    formButtons.className = 'form-buttons';
-    
-    const submitButton = document.createElement('button');
-    submitButton.type = 'submit';
-    submitButton.className = 'btn-primary';
-    submitButton.textContent = 'Add Task';
-    
-    const cancelButton = document.createElement('button');
-    cancelButton.type = 'button';
-    cancelButton.className = 'btn-secondary';
-    cancelButton.id = 'cancel-btn';
-    cancelButton.textContent = 'Cancel';
-    
-    formButtons.append(submitButton, cancelButton);
-    taskForm.append(taskInputGroup, dateGroup, formButtons);
-    modalInner.append(modalTitle, taskForm);
-    modal.append(modalInner);
-
-    const body = document.querySelector('body');
-    body.append(modal);
-    
-    styleModal(modal);
-    
-    modal.style.display = 'block';
-    
-    cancelButton.addEventListener('click', closeModal);
-    
-    taskForm.addEventListener('submit', handleTaskSubmit);
-    
-    function closeModal(){
-        modal.style.display = 'none';
-        setTimeout(() => {
-            if (modal.parentNode) {
-                modal.parentNode.removeChild(modal);
-            }}, 300);
-    }
-    
-    function handleTaskSubmit(event){
-        event.preventDefault();
-        addTask();
-        closeModal();
-    }
-}
-
-let taskLocalList = JSON.parse(localStorage.getItem('taskLocalList')) || [];
-
-const addTask = () => {
-    const task = document.querySelector('#inputTask').value.trim();
-    const date = document.querySelector('#inputDate').value;
-
-    taskLocalList.push({
-        id: Date.now().toString(),
-        task: task,
-        date: date,
-        completed: false
-    });
-
-    localStorage.setItem('taskLocalList', JSON.stringify(taskLocalList));
-    displayTasks();
-    document.querySelector("form").reset();
-}
-
-
 const displayTasks = () => {
     const tasksTable = document.querySelector('#todo-tbody');
 
@@ -297,16 +107,16 @@ const displayTasks = () => {
         taskActions.className = 'task-actions';
 
         const deleteIcon = document.createElement('button');
-        const deleteImg = document.createElement('img');
+        const deleteImg = document.createElement('div');
         deleteIcon.className = 'task-delete';
-        deleteImg.src = 'images/delete.png';
+        deleteImg.className = 'fa-regular fa-trash-can';
         
         deleteIcon.addEventListener('click', () => deleteTask(element.id));
 
         const editIcon = document.createElement('button');
-        const editImg = document.createElement('img');
+        const editImg = document.createElement('div');
         editIcon.className = 'task-edit';
-        editImg.src = 'images/edit.png';
+        editImg.className = 'fa-solid fa-pen';
         
         editIcon.addEventListener('click', () => editTaskDate(element.id));
 
---
+++ sidebar.js
@@ -0,0 +1,189 @@
+const createSidebar = () => {
+    const nav = document.createElement('nav');
+    nav.className = 'sidebar';
+
+    const header = document.createElement('header');
+    header.className = 'sidebarHeader'
+
+    const logo = document.createElement('div');
+    logo.className = 'sidebarLogo'
+
+    const logoText = document.createElement('div');
+    logoText.textContent = 'ToDo List';
+
+    const logoImg = document.createElement('img');
+    logoImg.src = 'images/favicon.png';
+
+    logo.append(logoImg, logoText);
+
+    header.append(logo);
+
+    const menuBar = document.createElement('div');
+    menuBar.className = 'sidebarMenu';
+
+    const menuItems = [
+        { id: 'add-task', text: 'add task', icon: 'fa-solid fa-plus'},
+        { id: 'search-task', text: 'search task', icon: 'fa-solid fa-magnifying-glass' },
+        { id: 'view-due-today', text: 'due today', icon: 'fa-regular fa-calendar' },
+        { id: 'view-upcoming', text: 'upcoming', icon: 'fa-regular fa-calendar-days' },
+    ];
+
+    menuItems.forEach(item => {
+        const menuItem = document.createElement('div');
+        menuItem.className = 'sidebarMenuItem';
+        menuItem.id = item.id;
+        
+        const menuIcon = document.createElement('span');
+        menuIcon.className = 'menuIcon';
+        menuIcon.className = item.icon;
+        
+        const menuText = document.createElement('span');
+        menuText.className = 'menuText';
+        menuText.textContent = item.text;
+        
+        menuItem.append(menuIcon, menuText);
+        menuBar.append(menuItem);
+
+        addMenuItemEventListeners(menuItem, item.id);
+    });
+
+    nav.append(header, menuBar);
+
+    styleSidebar(nav);
+    styleSidebarHeader(header);
+    styleSidebarLogo(logo);
+    styleSideBarImg(logoImg);
+    styleSidebarMenu(menuBar);
+    const menuItemElements = nav.querySelectorAll('.sidebarMenuItem');
+    styleSidebarMenuItem(menuItemElements);
+    return nav;
+}
+
+function addMenuItemEventListeners(menuItem, itemID){
+    menuItem.addEventListener('click', () => {
+        switch(itemID) {
+            case 'add-task':
+                openAddTaskModal();
+                break;
+            case 'search-task':
+                openSearchModal();
+                break;
+            case 'view-due-today':
+                showDueTodayTasks();
+                break;
+            case 'view-upcoming':
+                showUpcomingTasks();
+                break;
+        }
+    })
+}
+
+const openAddTaskModal = () => {
+    const modal = document.createElement('div');
+    modal.className = 'addTaskModal';
+    modal.id = 'add-task-modal';
+    
+    const modalInner = document.createElement('div');
+    modalInner.className = 'modalInner';
+    
+    const modalTitle = document.createElement('h2');
+    modalTitle.textContent = 'Add New Task';
+
+    const taskForm = document.createElement('form');
+    taskForm.id = 'task-form';
+    
+    const taskInputGroup = document.createElement('div');
+    taskInputGroup.className = 'formElements';
+    
+    const taskLabel = document.createElement('label');
+    taskLabel.setAttribute('for', 'inputTask');
+    taskLabel.textContent = 'Enter a task:';
+    
+    const taskInput = document.createElement('input'); 
+    taskInput.type = 'text';
+    taskInput.id = 'inputTask';
+    taskInput.setAttribute('autocomplete', 'off');
+    taskInput.setAttribute('required', 'true');
+    taskInput.placeholder = 'Enter your task here';
+    
+    taskInputGroup.append(taskLabel, taskInput);
+    
+    const dateGroup = document.createElement('div');
+    dateGroup.className = 'formElements';
+    
+    const dateLabel = document.createElement('label');
+    dateLabel.setAttribute('for', 'inputDate');
+    dateLabel.textContent = 'Due Date:';
+    
+    const inputDate = document.createElement('input');
+    inputDate.type = 'date';
+    inputDate.id = 'inputDate';
+    inputDate.name = 'task-date';
+    inputDate.required = true;
+    inputDate.setAttribute('autocomplete', 'off');
+    
+    dateGroup.append(dateLabel, inputDate);    
+
+    const formButtons = document.createElement('div');
+    formButtons.className = 'form-buttons';
+    
+    const submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+    submitButton.className = 'btn-primary';
+    submitButton.textContent = 'Add Task';
+    
+    const cancelButton = document.createElement('button');
+    cancelButton.type = 'button';
+    cancelButton.className = 'btn-secondary';
+    cancelButton.id = 'cancel-btn';
+    cancelButton.textContent = 'Cancel';
+    
+    formButtons.append(submitButton, cancelButton);
+    taskForm.append(taskInputGroup, dateGroup, formButtons);
+    modalInner.append(modalTitle, taskForm);
+    modal.append(modalInner);
+
+    const body = document.querySelector('body');
+    body.append(modal);
+    
+    styleModal(modal);
+    
+    modal.style.display = 'block';
+    
+    cancelButton.addEventListener('click', closeModal);
+    
+    taskForm.addEventListener('submit', handleTaskSubmit);
+    
+    function closeModal(){
+        modal.style.display = 'none';
+        setTimeout(() => {
+            if (modal.parentNode) {
+                modal.parentNode.removeChild(modal);
+            }}, 300);
+    }
+    
+    function handleTaskSubmit(event){
+        event.preventDefault();
+        addTask();
+        closeModal();
+    }
+}
+
+let taskLocalList = JSON.parse(localStorage.getItem('taskLocalList')) || [];
+
+const addTask = () => {
+    const task = document.querySelector('#inputTask').value.trim();
+    const date = document.querySelector('#inputDate').value;
+
+    taskLocalList.push({
+        id: Date.now().toString(),
+        task: task,
+        date: date,
+        completed: false
+    });
+
+    localStorage.setItem('taskLocalList', JSON.stringify(taskLocalList));
+    displayTasks();
+    document.querySelector("form").reset();
+}
+
